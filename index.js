9558e725e2f89aa94c503508993c88c5153b0e65
elkarroumytaha <elkarroumytaha@gmail.com>
2022-04-13 01:51:07 +0000

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+var http = require('http'); // Import Node.js core module
+var platinum = require("./moduls/App.js");
+
+var app = new platinum();
+
+app.routers = {
+    "/posts" : 
+
+}
+
+
+
+
+
+
+
+app.listentoPort(8080);
